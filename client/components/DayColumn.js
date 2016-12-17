45e7f6dc4e97561d3d79b2fa8d2cd385ad4b1093
Josh Terry <joshterry55@gmail.com>
2016-12-17 12:48:59 -0700

    commented out the highlight today code

--- client/components/DayColumn.js
+++ client/components/DayColumn.js
@@ -2,14 +2,24 @@ import React, { Component} from 'react';
 import { connect } from 'react-redux';
 
 class DayColumn extends Component {
-	
+
 
 	myDate() {
+
 		let dayOffset = this.props.setdate - this.props.day;
 		let fullDate = new Date(Date.now()-((dayOffset * 24)*60*60*1000));
 		let dateLength = fullDate.toDateString();
 		let myDay = fullDate.toDateString().substr(0, dateLength.length - 5)
 		// let myDay = (fullDate.getMonth() + 1) + "/" + fullDate.getDate() + "/" + fullDate.getFullYear()
+
+
+		// if(dayOffset === 0) {
+		// 	debugger
+		// 	$('#boxstyle').css('backgroundColor', 'red')
+		// } else {
+		// 	debugger
+		// 	$('#boxstyle').css('backgroundColor', 'lightblue')
+		// }
 		return myDay
 	}
 
@@ -18,7 +28,7 @@ class DayColumn extends Component {
 		let myDay = this.myDate();
 		return (
 			<div>
-				<div style={styles.dateBox}>{myDay}</div>
+				<div id='boxstyle' style={styles.dateBox}>{myDay}</div>
 
 			</div>
 		);
@@ -37,7 +47,7 @@ const styles = {
 		fontSize: '20px',
 		lineHeight: "38px",
 		border: "1px solid blue",
-		backgroundColor: "lightblue",
+		backgroundColor: 'lightblue',
 		float: 'left'
 	}
 }
