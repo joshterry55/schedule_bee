4cba5c93dcc51156dc77ffaea25f193de7e413b7
Josh Terry <joshterry55@gmail.com>
2016-12-16 22:36:09 -0700

    refactor

--- client/reducers/setdate.js
+++ client/reducers/setdate.js
@@ -5,18 +5,10 @@ const setdate = (state = {}, action) => {
 			let numberOfDay = today.getDay();
 			return numberOfDay;
 		case "SET_WEEK":
-			if(action.initial === 1) {
-				let today1 = new Date()
-				let number = today1.getDay();
-				let numberOfDay1 = number + 7
-				return numberOfDay1;
-			}	else {
 				let today2 = new Date()
 				let number2 = today2.getDay();
 				let numberOfDay2 = (number2 + (7 * action.initial))
 				return numberOfDay2;
-				debugger
-			}
 		default:
 			return state;
 	}
