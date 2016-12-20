87abca5ab05ca09fb7aa3feae0efa2697e07763a
Ali Jepsen <ali.jepsen@gmail.com>
2016-12-20 16:07:51 -0700

    production file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,7 +86,7 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: "schedule-bee.herokuapp.com" }
 
-  config.action_mailer.deliver_method :smtp
+  config.action_mailer.delivery_method :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
