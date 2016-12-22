a307fd53d818ca014a79dde4fdd48f2f8ff760ae
Ali Jepsen <ali.jepsen@gmail.com>
2016-12-22 15:50:07 -0700

    adds mailer error catching to prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,7 +86,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { host: "schedule-bee.herokuapp.com" }
-
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
