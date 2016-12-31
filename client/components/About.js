d2306c41e93c4a146cd01300301e7a5940f1e061
Marisa Jense <marisajense@gmail.com>
2016-12-30 17:22:36 -0700

    about update added li and github links

--- client/components/About.js
+++ client/components/About.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 class About extends React.Component {
 
@@ -7,58 +8,123 @@ class About extends React.Component {
       <div className='container'>
         <div>
         <h3>About ScheduleBee</h3>
-        <p><strong>ScheduleBee</strong> was created due to the lack of easy-to-use scheduling apps on the market currently.
+        <p style={styles.about}><strong style={styles.name}>ScheduleBee</strong> was created due to the lack of easy-to-use scheduling apps on the market currently.
           Cael Jensen thought of the idea, having mangaged and scheduled employees in the past. Cael wanted an easier way to
          create a schedule as well as to keep track of employee hours to ensure his employees were getting enough hours and not working overtime.</p>
        <hr/>
        </div>
-       <div className="center row about-page">
-         <h3 id='team'>The Team</h3>
-         <div className="col s12 m6">
-           <h5>Cael Jensen</h5>
-           <span className="cael-pic"></span>
-           <p>Cael is an entrepreneurial-minded web developer who has been tinkering
+       <div className="center row about-page" >
+         <h3>The Team</h3>
+
+         <div className="col s12 m6 individual">
+           <span style={styles.pic} className="cael-pic"></span>
+           <h5 style={styles.team}>Cael Jensen</h5>
+           <p style={styles.bio}>Cael is an entrepreneurial-minded web developer who has been tinkering
               with computers all his life. Creativity and design are his driving passions,
-              and he's always looking for ways to expand his knowledge and experience.</p>
+            and he's always looking for ways to expand his knowledge and experience.</p>
+          <Link style ={styles.button} to='https://www.linkedin.com/in/cael-jensen-58b1b6134' target='_blank'><img className="li-pic"/></Link>
+          <Link style ={styles.button} to='https://github.com/crjmosh' target='_blank'><img className="github-pic"/></Link>
          </div>
-         <div className="col s12 m6">
-           <h5>Josh Terry</h5>
-           <span className="josh-pic"></span>
-           <p>Josh testing testingtestingtestingtestingtestingtesting, testingtestingtestingtestingtestingtestingtesting.
-              testingtestingtestingtesting testingtestingtestingtesting testingtestingtestingtesting testingtestingtestingtesting.
-              testingtest.</p>
+
+         <div className="col s12 m6 individual">
+           <span style={styles.pic} className="josh-pic"></span>
+           <h5 style={styles.team}>Josh Terry</h5>
+           <p style={styles.bio}>Josh stuff here. Need more info! Waiting for more info Josh. You can do it! Blah blah Blah
+              Blah blah blah blah blaaaaaa.</p>
+            <Link style ={styles.button} to='https://www.linkedin.com/in/josh-terry-12b55311b' target='_blank'><img className="li-pic"/></Link>
+            <Link style ={styles.button} to='https://github.com/joshterry55' target='_blank'><img className="github-pic"/></Link>
          </div>
-         <div className="col s12 m6">
-           <h5>Ali Jepson</h5>
-           <span className="ali-pic"></span>
-           <p>testingtestingtestingtesting. testingtestingtestingtesting. testingtestingtestingtesting. testingtestingtesting.
-              testingtestingtestingtestingtesting. testingtestingtestingtesting. testingtestingtesting. testing testing
-              testingtestingtestingtestingtestingtestingtestingtestingtesting.</p>
+
+         <div className="col s12 m6 individual">
+           <span style={styles.pic} className="ali-pic"></span>
+           <h5 style={styles.team}>Ali Jepsen</h5>
+           <p style={styles.bio}>Ali has been working in the software industry for almost 20 years primarily in product management and QA testing.</p>
+           <Link style ={styles.button} to='https://www.linkedin.com/in/ali-jepsen-879639' target='_blank'><img className="li-pic"/></Link>
+           <Link style ={styles.button} to='https://github.com/alijepsen' target='_blank'><img className="github-pic"/></Link>
          </div>
-         <div className="col s12 m6">
-           <h5>Marisa Jense</h5>
-           <span className="marisa-pic"></span>
-           <p>Marisa's background is in behavioral science, but
+
+         <div className="col s12 m6 individual">
+           <span style={styles.pic} className="marisa-pic"></span>
+           <h5 style={styles.team}>Marisa Jense</h5>
+           <p style={styles.bio}>Marisa's background is in behavioral science, but
               her passion for creativity and desire to learn carried her into the tech world.
-              She is excited for her future in web development as it has proven to be ever challenging and extremely rewarding.</p>
+              She is excited for her future in web development as it has proven to be ever
+              challenging and extremely rewarding.</p>
+            <Link style ={styles.button} to='https://www.linkedin.com/in/marisa-jense-332930aa' target='_blank'><img className="li-pic"/></Link>
+            <Link style ={styles.button} to='https://github.com/marisajense' target='_blank'><img className="github-pic"/></Link>
          </div>
        </div>
+
        <div>
          <h3>Languages & Tools</h3>
          <hr/>
          <div className="center row">
            <div className='col s12 m4'>
+             <h5>Ruby on Rails</h5>
+             <span className="rails-pic"></span>
+           </div>
+           <div className='col s12 m4'>
            <h5>React</h5>
            <span className="react-pic"></span>
            </div>
            <div className='col s12 m4'>
-           <h5>Ruby on Rails</h5>
-           <span className="rails-pic"></span>
+             <h5>Redux</h5>
+           <span className="redux-pic"></span>
+           </div>
+           <div className='col s12 m4'>
+           <span className="html-pic"></span>
+           </div>
+           <div className='col s12 m4'>
+             <span className="css3-pic"></span>
+           </div>
+           <div className='col s12 m4'>
+           <span className="jquery-pic"></span>
+           </div>
+           <div className='col s12 m4'>
+             <h5>Materialize</h5>
+           <span className="material-pic"></span>
+           </div>
+           <div className='col s12 m4'>
+           <span className="devise-pic"></span>
+           </div>
+           <div className='col s12 m4'>
+           <span className="ajax-pic"></span>
            </div>
          </div>
        </div>
       </div>
     )
+  }
+}
+
+const styles = {
+  team: {
+    color: '#1565C0',
+    textShadow: '0 1px #ddd',
+  },
+  about: {
+    fontSize: '18px'
+  },
+  name: {
+    color: '#1565C0',
+    fontSize: '20px',
+    textShadow: '0 1px #ddd'
+  },
+  pic: {
+    boxShadow: '10px 10px 5px #eee',
+    float: 'left',
+    margin: '10px',
+    marginTop: '20px'
+  },
+  bio: {
+    fontSize: '16px',
+    textAlign: 'left',
+  },
+  li: {
+    height: '30px',
+  },
+  button: {
+    float: 'right',
   }
 }
 
