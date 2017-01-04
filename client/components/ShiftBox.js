62115cb59117e9a28c41121838121b4cf36232dd
Josh Terry <joshterry55@gmail.com>
2017-01-03 19:09:48 -0700

    fixed conflict

--- client/components/ShiftBox.js
+++ client/components/ShiftBox.js
@@ -40,25 +40,35 @@ class ShiftBox extends React.Component {
 	shiftModal() {
 
 		let date = this.props.shiftdate
-		let employeeId = this.props.id
+		let employeeId = this.props.currentemployee
 		let company = this.props.setcompany
+		let employeeName;
+		this.props.setemployee.map( employee => {
+			if(employee.id == employeeId) {
+				employeeName = `${employee.first_name} ${employee.last_name}`
+			}
+		})
+
 		return(
 			<div>
+				<div style={styles.modalHeader}>
+					<span>Add Shift</span>
+					<span style={styles.modalHeaderInfo}>{date}<br />{employeeName}</span>
+				</div>
 				<div className="modal-content">
-					<h4>{date}</h4>
 					<form className='row'>
-						<div className='col s3'>
-							<label>Start</label>
-							<input type='time' ref='shiftStart' autofocus />
+						<div className='col s6 m4 offset-m2'>
+							<label>Start Time</label>
+							<input type='time' ref='shiftStart' />
 						</div>
-						<div className='col s3'>
-							<label>End</label>
+						<div className='col s6 m4'>
+							<label>End Time</label>
 							<input type='time' ref='shiftEnd' />
 						</div>
 					</form>
 				</div>
-				<div className="modal-footer">
-					<a href="#!" onClick={this.submitShift} className=" modal-action modal-close waves-effect waves-green btn-flat">Agree</a>
+				<div className="modal-footer" style={styles.modalFooter}>
+					<a href="#!" onClick={this.submitShift} className=" modal-action modal-close waves-effect waves-green btn-flat">Add</a>
 				</div>
 			</div>
 		)
@@ -66,27 +76,36 @@ class ShiftBox extends React.Component {
 
 	editShiftModal() {
 		let date = this.props.shiftdate
-		let employeeId = this.props.id
+		let employeeId = this.props.currentemployee
 		let shifts = this.props.shiftedit
 		let company = this.props.setcompany
+		let employeeName;
+		this.props.setemployee.map( employee => {
+			if(employee.id == employeeId) {
+				employeeName = `${employee.first_name} ${employee.last_name}`
+			}
+		})
 
 		return(
 			<div>
+				<div style={styles.modalHeader}>
+					<span>Edit Shift</span>
+					<span style={styles.modalHeaderInfo}>{date}<br />{employeeName}</span>
+				</div>
 				<div className="modal-content">
-					<h4>{date}</h4>
 					<form className='row'>
-						<div className='col s3'>
+						<div className='col s6 m4 offset-m2'>
 							<label>Start</label>
-							<input type='time' ref='editShiftStart' autofocus />
+							<input type='time' ref='editShiftStart' />
 						</div>
-						<div className='col s3'>
+						<div className='col s6 m4'>
 							<label>End</label>
 							<input type='time' ref='editShiftEnd' defaultValue={shifts.end}/>
 						</div>
 					</form>
 				</div>
-				<div className="modal-footer">
-					<a href="#!" onClick={this.submitEditShift} className=" modal-action modal-close waves-effect waves-green btn-flat">Agree</a>
+				<div className="modal-footer" style={styles.modalFooter}>
+					<a href="#!" onClick={this.submitEditShift} className=" modal-action modal-close waves-effect waves-green btn-flat">Update</a>
 				</div>
 			</div>
 		)
@@ -288,10 +307,10 @@ class ShiftBox extends React.Component {
 		return(
 			<div>
 				{this.display()}
-				<div id="modal1" className="modal">
+				<div id="modal1" className="modal" style={styles.modalStyling}>
 					{this.shiftModal()}
 				</div>
-				<div id="modal2" className="modal">
+				<div id="modal2" className="modal" style={styles.modalStyling}>
 					{this.editShiftModal()}
 				</div>
 			</div>
@@ -365,12 +384,46 @@ const styles = {
 		position: "absolute",
 		top: "-2px",
 		right: "-5px",
+	},
+	modalStyling: {
+		height: '220px',
+		width: '80%',
+		maxWidth: '500px',
+		border: '1px solid #333',
+		borderRadius: '15px'
+	},
+	modalFooter: {
+		position: 'absolute',
+		bottom: '0px',
+	},
+	modalHeader: {
+		width: '100%',
+		height: '60px',
+		lineHeight: '61px',
+		color: '#fff',
+		fontSize: '35px',
+		textShadow: '0 0 5px rgba(0,0,0,0.50)',
+		backgroundColor: "#66f",
+		background: "linear-gradient(#1b7ff2, #1257a6)",
+		borderBottom: '1px solid #333',
+		boxShadow: '0 0 6px #000',
+		position: 'relative',
+		paddingLeft: '10px'
+	},
+	modalHeaderInfo: {
+		color: '#fff',
+		fontSize: '20px',
+		lineHeight: '30px',
+		position: 'absolute',
+		right: '10px',
+		textAlign: 'right',
+		textShadow: '0 0 5px rgba(0,0,0,0.50)'
 	}
 }
 
 const mapStateToProps = (state) => {
-  let { user, assigned, setcompany, currentemployee, shiftdate, showshift, currentshifts, shiftedit } = state;
-  return { user, assigned, setcompany, currentemployee, shiftdate, showshift, currentshifts, shiftedit }
+  let { user, assigned, setcompany, currentemployee, shiftdate, showshift, currentshifts, shiftedit, setemployee } = state;
+  return { user, assigned, setcompany, currentemployee, shiftdate, showshift, currentshifts, shiftedit, setemployee }
 }
 
 export default connect(mapStateToProps)(ShiftBox);
