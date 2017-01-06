363a4baca01ed03494458bd94aacd16ebf11c0ca
Ali Jepsen <ali.jepsen@gmail.com>
2017-01-06 12:05:18 -0700

    try new image

--- client/components/EmployeeInfo.js
+++ client/components/EmployeeInfo.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import DropZone from 'react-dropzone';
 import request from 'superagent';
-import changeImage from '../images/changeImage.jpg';
+import changeImage from '../images/changeImageDownload.png';
 require('superagent-rails-csrf')(request)
 
 class EmployeeInfo extends React.Component {
