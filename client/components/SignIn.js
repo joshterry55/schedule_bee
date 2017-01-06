d05daa081a56f97133d0598fda44cc7003076fff
Cael Jensen <crjmosh@gmail.com>
2017-01-05 17:58:54 -0700

    sign in page semi styled

--- client/components/SignIn.js
+++ client/components/SignIn.js
@@ -34,17 +34,19 @@ class SignIn extends React.Component {
 
   render() {
     return(
-      <div className='container row' style={styles.signIn}>
-        <h3 className='center'>Sign In</h3>
-        <form onSubmit={this.handleSubmit}>
-          <div className='col s8 offset-s2'>
-            <input type="email" required={true} ref='email' placeholder='Email' />
-            <input type='password' required={true} ref='password' placeholder='Password' />
-          </div>
-          <div className='col s12 center'>
-            <button className='btn blue darken-3'>Sign In</button>
-          </div>
-        </form>
+      <div className='container row'> 
+        <div style={styles.signIn} className="col s12 m10 offset-m1 center">
+          <h3>Sign In</h3>
+          <form onSubmit={this.handleSubmit}>
+            <div className='col s8 offset-s2'>
+              <input type="email" required={true} ref='email' placeholder='Email' />
+              <input type='password' required={true} ref='password' placeholder='Password' />
+            </div>
+            <div className='col s12 center'>
+              <button className='btn blue darken-3'>Sign In</button>
+            </div>
+          </form>
+        </div>
       </div>
     )
   }
@@ -53,8 +55,9 @@ class SignIn extends React.Component {
 
 const styles={
   signIn: {
-    backgroundColor: '#E9EAED',
-    width: '800px',
+    backgroundColor: '#E9E9E9',
+    borderRadius: '15px',
+    boxShadow: '5px 5px 5px rgba(0,0,0,0.5)',
     height: '300px',
     padding: '5px',
     marginTop: '20px',
