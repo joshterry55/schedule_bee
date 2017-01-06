6df6a5f92bc333e0942060f1b2201b494b3b0bd4
Ali Jepsen <ali.jepsen@gmail.com>
2017-01-06 12:10:41 -0700

    take out img opt

--- config/webpack.config.heroku.js
+++ config/webpack.config.heroku.js
@@ -48,7 +48,7 @@ var config = {
         test: /\.(jpe?g|png|gif|svg)$/i,
         loaders: [
             'file?hash=sha512&digest=hex&name=[hash].[ext]',
-            'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+            'image-webpack'
         ]
       }
     ]
